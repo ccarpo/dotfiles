d1986cadfa6e37c9e0853d505f7ccbab736a7a5b
CCarpo <ccarpo@gmx.net>
2023-07-25 09:57:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,9 +33,9 @@ if [ -d ~/dotfiles ]; then
 fi
 git clone -b p10k https://github.com/ccarpo/dotfiles.git --recurse-submodules ~/dotfiles
 cp -rf ~/dotfiles/.* ~
-sed -i -e 's/<<replaceuser>>/$(whoami)/g' ~/.zshrc
+sed -i -e 's/<<replaceuser>>/'"$(whoami)"'/g' ~/.zshrc
 
-#TODO ask if you wnat to install it for this user only then use ~/.local/share/fonts instead
+#TODO ask if you want to install it for this user only then use ~/.local/share/fonts instead
 echo "install p10k fonts"
 sudo curl -o /usr/share/fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 sudo curl -o /usr/share/fonts/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
@@ -45,4 +45,4 @@ sudo curl -o /usr/share/fonts/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/
 echo "Install PowerLevel10k theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-#p10k configure
+#TODO use preconfigured p10k rc file
